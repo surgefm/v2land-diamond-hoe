ef06fcb6c6284fcaf4fc79f43742395f2bbcafb7
IncredibLink <link@langchao.org>
2020-01-04 15:28:31 +0800

    Implement the PriorityQueue for proxy pools

---
+++ src/utils/PriorityQueue.ts
@@ -0,0 +1,78 @@
+export interface PriorityQueueOptions<inputType> {
+  priority?: number;
+  validate?: (input: inputType) => boolean;
+}
+
+class Node<valueType, inputType> {
+  value: valueType;
+  next: Node<valueType, inputType> = null;
+  priority: number = 1;
+  validate: ((input?: inputType) => boolean) = () => true;
+  
+  constructor(value: valueType, options: PriorityQueueOptions<inputType> = {}) {
+    this.value = value;
+    if (typeof options.priority !== 'undefined') {
+      this.priority = options.priority;
+    }
+    if (typeof options.validate !== 'undefined') {
+      this.validate = options.validate;
+    }
+  }
+}
+
+export class PriorityQueue<valueType, inputType> {
+  first: Node<valueType, inputType> = null;
+  last: Node<valueType, inputType> = null;
+  length: number = 0;
+
+  push(value: valueType, options: PriorityQueueOptions<inputType> = {}): void {
+    const node = new Node<valueType, inputType>(value, options);
+
+    if (this.last === null) {
+      this.first = node;
+      this.last = node;
+    } else if (typeof options.priority === 'undefined') {
+      this.last.next = node;
+      this.last = node;
+    } else if (node.priority > this.first.priority) {
+      node.next = this.first;
+      this.first = node;
+    } else { 
+      let previous = this.first;
+      while (previous.next !== null && previous.next.priority >= node.priority) {
+        previous = previous.next;
+      }
+      node.next = previous.next;
+      previous.next = node;
+    }
+
+    this.length++;
+    return;
+  }
+
+  poll(input?: inputType): valueType {
+    let previous: Node<valueType, inputType> = null;
+    let node = this.first;
+
+    while (node !== null) {
+      if (node.validate(input)) {
+        if (previous !== null) {
+          previous.next = node.next;
+        } else {
+          this.first = node.next;
+        }
+
+        if (node === this.last) this.last = previous;
+
+        const value = node.value;
+        node = null;
+        return value;
+      }
+
+      previous = node;
+      node = node.next;
+    }
+
+    return null;
+  }
+}
