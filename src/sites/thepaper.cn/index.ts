352874caf7a2bab374bb31b4617d0b9e6601b37b
Peking Duck <link@langchao.org>
2020-01-05 21:57:17 +0800

    Minor format changes and mobile site support

--- src/sites/thepaper.cn/index.ts
+++ src/sites/thepaper.cn/index.ts
@@ -1,6 +1,6 @@
 import { SiteObj, Crawler } from '@Types';
 import { Article } from '@Models';
-import { checkArticleWithURL, safe } from '@Utils';
+import { checkArticleWithURL, safe, removeURLQuery, getCrawlerWithDomain } from '@Utils';
 import { Page, ElementHandle } from 'puppeteer';
 import delay from 'delay';
 import * as _ from 'lodash';
@@ -9,17 +9,33 @@ export const thepaperCn: SiteObj = {
   name: '澎湃新闻',
   domains: [
     'www.thepaper.cn',
+    'm.thepaper.cn',
   ],
 };
 
+export class MThepaperCnCrawler extends Crawler {
+  site = thepaperCn;
+  domains = ['m.thepaper.cn'];
+ 
+  async crawlArticle(page: Page: url: string): Promise<[Article, boolean]> {
+    const Url = new URL(url);
+    Url.hostname = 'www.thepaper.cn';
+    const crawler = await getCrawlerWithDomain('www.thepaper.cn');
+    return crawler.crawlArticle(page, Url.toString());
+  }
+}
+
 export class ThepaperCnCrawler extends Crawler {
   site = thepaperCn;
+  domains = ['www.thepaper.cn'];
 
   public async init(): Promise<Crawler> {
     return super.init(2);
   }
 
   async crawlArticle(page: Page, url: string): Promise<[Article, boolean]> {
+    url = removeURLQuery(url);
+
     const [article, proceed] = await checkArticleWithURL(thepaperCn, url);
     if (!proceed) return [article, false];
 
@@ -29,11 +45,11 @@ export class ThepaperCnCrawler extends Crawler {
       article.url = url;
       article.html = await page.content();
       article.source = null; // all thepaper.cn articles are original
-      
+
       function extractTime(text: String): Date {
         let regExp = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/;
         let index = text.search(regExp);
-        if(index > 0) {
+        if (index > 0) {
           let timeString = text.substring(index, index + 16);
           return new Date(timeString + ' GMT+8');
         } else {
@@ -42,19 +58,18 @@ export class ThepaperCnCrawler extends Crawler {
       }
 
       // for video news
-      if(await page.$('.video_news_detail')) {
+      if (await page.$('.video_news_detail')) {
         article.title = await page.$eval('.video_txt_detail .video_txt_t h2', el => el.textContent.trim());
         article.abstract = await page.$eval('.video_txt_detail .video_txt_l p', el => el.textContent.trim());
         article.content = article.abstract;
         let timeHTML = await page.$eval('.video_info_first .video_info_left', el => el.innerHTML);
         article.time = extractTime(timeHTML);
-
       // for text news
       } else {
         article.title = await page.$eval('.newscontent .news_title', el => el.textContent.trim());
         article.content = await page.$eval(
           '.newscontent .news_txt', 
-          el => Array.from(el.childNodes).map(x=>x.textContent.trim()).filter(t => t.length !== 0).join('\n')
+          el => Array.from(el.childNodes).map(x=>x.textContent.trim()).filter(t => t.length !== 0).join('\n'),
         );
         article.abstract = article.content.substring(0, 200); // thepaper.cn has no abstract; use content as abstract
         let timeHTML = await page.$eval('.newscontent .news_about', el => el.innerHTML);
@@ -62,7 +77,6 @@ export class ThepaperCnCrawler extends Crawler {
       }
 
       return [await article.save(), true];
-
     } catch (err) {
       article.status = 'pending';
       return [await article.save(), false];
@@ -84,11 +98,10 @@ export class ThepaperCnCrawler extends Crawler {
 
       let paths = await page.$$eval(
         '#mainContent .newsbox .news_li>h2>a',
-        els => els.map(el => el.getAttribute('href'))
+        els => els.map(el => el.getAttribute('href')),
       );
       let urls = paths.map(path => page.url() + path);
       return _.uniq(urls);
-
     } catch (err) {
       return [];
     }
